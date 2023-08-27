549301627b8e63149811beb870a9885b6ba5f7c1
Shohjahon <130474531+oktamovshohjahon@users.noreply.github.com>
2023-08-27 14:59:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "6259166096:AAEFqGGloZM_6krr2A9R3lIWMOOCjRTCCQk";
+const token = "YourToken";
 
 const bot = new TelegramBot(token, { polling: true });
 
